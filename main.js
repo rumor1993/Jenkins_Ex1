93649ea0a8ddd307ca6376e38268ff74d1397534
rumor1993 <rumor1993@naver.com>
2019-07-27 12:37:06 +0900

    650

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ function createWindow () {
   // Create the browser window.
   mainWindow = new BrowserWindow({
     width: 800,
-    height: 600,
+    height: 650,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
     }
